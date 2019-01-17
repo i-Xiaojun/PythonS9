b4250f5c1ab47f6fa8aeec3c60f1f952764bebde
Harry_Zhou <584435675@qq.com>
2019-01-17 16:38:51 +0800

    first commit

---
+++ Day16/9.作业.py
@@ -0,0 +1,27 @@
+# 1. 整理内置函数的思维导图和博客
+#
+# 2. 红色, 黄色方法敲一遍
+#
+# 3. 用map来处理字符串列表,把列表中所有人都变成sb, 例如alex_sb
+# name=['alex','wupeiqi','yuanhao','nezha']
+#
+#  4. 用filter函数处理数字列表,将列表中所有的偶数筛选出来
+# num=[1,3,5,6,7,8]
+#
+# 5. 随意写一个20行以上的文件,运行程序,先将内容读到内存中,用列表存储
+# 接收用户输入页码,每页5条,仅输出当页的内容
+#
+# 6. 如下,每个小字典的name对应股票名字, shares对应多少股, price对应股票的价格
+# portfolio = [
+#     {'name': 'IBM', 'shares':100, 'price':91.1},
+#     {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+#     {'name': 'FB', 'shares': 200, 'price': 21.09},
+#     {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+#     {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+#     {'name': 'ACME', 'shares': 75, 'price': 115.65},
+# ]
+# 6.1 计算购买每只股票的总价
+# 6.2 用filter过滤输出,单价大于100的股票有哪些
+
+# No.3
+name=['alex','wupeiqi','yuanhao','nezha']
