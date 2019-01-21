5c25866d1319e0ca04b6ca196268ac758bb02fa9
Harry_Zhou <584435675@qq.com>
2019-01-21 18:05:09 +0800

    Day11 装饰器update

--- Day11/1.装饰器.py
+++ Day11/1.装饰器.py
@@ -33,21 +33,62 @@
 #     time.sleep(0.1)
 #
 # func()
-
-import time
-
-def func():
-    print('I`m ABC')
-    time.sleep(0.1)
-
-def mytimer():
-    #def inner():
-        start = time.time()
-        func()
-        end = time.time()
-        print(end-start)
-    #return inner
+#
+'''
+[问题]
+1. mytimer一定需要定义在func之上, 但实际是先写func函数,再写mytimer函数
+2.如果跨文件进行调用和定义该如何进行
+'''
 
 
-func = mytimer()
-func()
+
+# 装饰器带参数实现
+# def myprint(func):
+#     def inner(*args,**kwargs):
+#         print('Befor=======')
+#         func(*args,**kwargs)
+#         print('After*******')
+#     return inner
+#
+# @myprint  #func = myprint(func)
+# def func(*args,**kwargs):
+#     print([i for i in args])
+#
+# func(1,2)
+
+# 装饰器带返回值实现
+# def myprint(func):
+#     def inner(*args,**kwargs):
+#         print('Befor=======')
+#         ret = func(*args,**kwargs)
+#         print('After*******')
+#         return ret
+#     return inner
+#
+# @myprint  #func = myprint(func)
+# def func(*args,**kwargs):
+#     return [i for i in args]
+#
+# li = func(1,2)
+# print(li)
+# print(func.__name__)
+
+# 装饰器实现可以查看内置方法
+# from functools import wraps
+#
+# def myprint(func):
+#     @wraps(func)
+#     def inner(*args,**kwargs):
+#         print('Befor=======')
+#         ret = func(*args,**kwargs)
+#         print('After*******')
+#         return ret
+#     return inner
+#
+# @myprint  #func = myprint(func)
+# def func(*args,**kwargs):
+#     return [i for i in args]
+#
+# li = func(1,2)
+# print(li)
+# print(func.__name__)
---
+++ Day11/9.Day11_作业.py
@@ -0,0 +1,38 @@
+# 1.编写装饰器，为多个函数加上认证的功能（用户的账号密码来源于文件）, 要求登录成功一次，后续的函数都无需再输入用户名和密码
+# 2.编写装饰器，为多个函数加上记录调用功能，要求每次调用函数都将被调用的函数名称写入文件
+# 进阶作业(选做)：
+# 1.编写下载网页内容的函数，要求功能是：用户传入一个url，函数返回下载页面的结果
+# 2.为题目1编写装饰器，实现缓存网页内容的功能：
+# 具体：实现下载的页面存放于文件中，如果文件内有值（文件大小不为0），就优先从文件中读取网页内容，否则，就去下载，然后存到文件中
+
+# 1.编写装饰器，为多个函数加上认证的功能（用户的账号密码来源于文件）, 要求登录成功一次，后续的函数都无需再输入用户名和密码
+def login():
+    def inner():
+        f = open('UserDB.txt')
+        for line in f:
+            print(line.strip())
+        f.close()
+        func1()
+        func2()
+
+def func1():
+    print('Welcome Func1')
+
+def func2():
+    print('Welcome Func2')
+
+def my_func():
+    while True:
+        num = input('Pls choice: ')
+        if num == '1':
+            func1()
+            return 1
+        elif num == '2':
+            func2()
+            return 2
+        elif num == 'q':
+            return 'Bye'
+        else:
+            print('Pls retry: ')
+
+my_func()
