158af28854b4e6ca8425aec80adf4c9643c81b15
Harry_Zhou <584435675@qq.com>
2019-01-21 15:58:16 +0800

    作业rename

--- Day11/1.装饰器.py
+++ Day11/1.装饰器.py
@@ -1,1 +1,53 @@
+# 装饰器的第一版实现
+# import time
 #
+# def func():
+#     print('I`m ABC')
+#     time.sleep(0.1)
+#
+# def mytimer(func):
+#     def inner():
+#         start = time.time()
+#         func()
+#         end = time.time()
+#         print(end-start)
+#     return inner
+#
+# func = mytimer(func)
+# func()
+
+# 装饰器的第二版实现
+# import time
+#
+# def mytimer(func):
+#     def inner():
+#         start = time.time()
+#         func()
+#         end = time.time()
+#         print(end-start)
+#     return inner
+#
+# @mytimer
+# def func():
+#     print('I`m ABC')
+#     time.sleep(0.1)
+#
+# func()
+
+import time
+
+def func():
+    print('I`m ABC')
+    time.sleep(0.1)
+
+def mytimer():
+    #def inner():
+        start = time.time()
+        func()
+        end = time.time()
+        print(end-start)
+    #return inner
+
+
+func = mytimer()
+func()
