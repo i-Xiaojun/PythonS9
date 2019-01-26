0f5a22f381e0a3e710da24b2addbc726e7476a6c
Harry_Zhou <584435675@qq.com>
2019-01-26 13:37:34 +0800

    Day12 员工信息表 update

--- Day12/9.Day12_大作业-员工信息表.py
+++ Day12/9.Day12_大作业-员工信息表.py
@@ -28,23 +28,112 @@
 
 
 
-# f = open('Member_Info.txt')
-# for line in f:
-#     li = line.strip().split(',')
-#     if 'IT' in li:
-#         print(li[1])
-# f.close()
+def filter_file(condition):
+    if '>' in condition:
+        li_con = condition.split('>')
+        val1 = li_con[0].strip()
+        val2 = li_con[1].strip()
+        with open('Member_Info.txt') as f:
+            for line in f:
+                li = line.split(',')
+                if int(li[dic[val1]]) > int(val2):
+                    # print(line)
+                    return li
+
+def show_item(selcect_str):
+    item_li = selcect_str.replace('select','').strip().split(',')
+    print(item_li)
+    return item_li
+
+def show_end(item_li,filter_result):
+    for item in item_li:
+        print(item)
+        print(filter_result[dic[item]],end=' ')
+
+dic = {'id':0, 'name':1, 'age':2, 'phone':3, 'job':4}
+sql_str = 'select name,age where age>22'
+li_sql = sql_str.strip().split('where')
+
+f = filter_file('age>22')
+item = show_item('select name,age ')
+
+show_end(item,f)
+
+
+
+
+
+# def my_file(filename):
+#     with open(filename) as f:
+#         for line in f:
+#             yield line
 #
+# def filter_file(condition):
+#     if '>' in condition:
+#         li_con = condition.split('>')
+#         val1 = li_con[0].strip()
+#         val2 = li_con[1].strip()
 #
-#
-# def get_name(tiaojian):
+#         dic[val1]
 #     pass
+#
+# dic = {'id':0, 'name':1, 'age':2, 'phone':3, 'job':4}
+#
+# mysql = 'select name,age where age>22'
+# li_sql = mysql.strip().split('where')
+#
+# item = li_sql[0].replace('select','').strip()
+# condition = li_sql[1].strip()
+#
+# print(item,condition)
+#
+#
+#
+# my_file('Member_Info.txt')
 
-def user_input():
-    my_sql = input('>>>')
-    sql_li = my_sql.split(' ')
-    i = 0
-    print([i for i in sql_li])
-    #print(sql_li[1])
-
-user_input()
+# def filter_file(item,op,content):
+#     f = open('Member_Info.txt')
+#     for line in f:
+#         li = line.strip().split(',')
+#         li_id = li[0]
+#         li_name = li[1]
+#         li_age = li[2]
+#         li_phone = li[3]
+#         li_job = li[4]
+#         if 'name' in item:
+#         if op == '=':
+#             pass
+#         if op == '>':
+#             pass
+#         if op == '<':
+#             pass
+#         if op == 'like':
+#             pass
+#
+#     f.close()
+# #
+# #
+# #
+# # def get_name(tiaojian):
+# #     pass
+#
+# def read_input():
+#     my_sql = input('>>>')
+#     sql_li = my_sql.split('where')
+#     print(sql_li)
+#     if 'select' in sql_li[0]:
+#         item = sql_li[0].split('select')
+#         my_select(item[1])
+#     else:
+#         return print('请以select开头')
+#
+#
+#
+# def my_select(s):
+#     if s.strip() == '*':
+#         return 1
+#     else:
+#         print(s.strip().split(','))
+#         return s.strip().split(',')
+#
+# read_input()
